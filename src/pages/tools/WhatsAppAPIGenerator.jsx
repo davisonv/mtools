7a7925b1fc7181480c76afc93a494ff7a74b4528
Rbrollo <rtbrollo@gmail.com>
2023-12-07 22:23:44 -0300

    labels nos inputs
    botão com a função de copiar texto que fica dentro do alert

--- src/pages/tools/WhatsAppAPIGenerator.jsx
+++ src/pages/tools/WhatsAppAPIGenerator.jsx
@@ -1,7 +1,7 @@
-import React, {useState} from "react";
-import { Formik, Form, Field, } from 'formik';
-import {Row, Col, Container, Button, Alert} from "react-bootstrap"
-import * as Yup from 'yup';
+import React, { useState } from "react";
+import { Formik, Form, Field } from "formik";
+import { Row, Col, Container, Button, Alert } from "react-bootstrap";
+import * as Yup from "yup";
 import "yup-phone";
 import styled from "styled-components";
 
@@ -9,53 +9,60 @@ const ErrorLabel = styled.div`
   font-size: 11px;
   color: red;
   margin-top: -10px;
-`
+`;
 
-function WhatsAppAPIGenerator () {
-  const [whatsLink, setWhatsLink] = useState('')
+function WhatsAppAPIGenerator() {
+  const [whatsLink, setWhatsLink] = useState("");
 
   const initialValues = {
-    phoneNumber: '',
-    message: '',
-  }
+    phoneNumber: "",
+    message: "",
+  };
 
   const LinkGeneratorSchema = Yup.object().shape({
-    phoneNumber: Yup.string().phone("IN", false, "Digite um número válido.").required(),
+    phoneNumber: Yup.string()
+      .phone("IN", false, "Digite um número válido.")
+      .required(),
     message: Yup.string()
-              .min(3, 'Digite uma mensagem mais longa!')
-                .max(60, 'Digite uma mensagem mais curta!').nullable()
-  })
+      .min(3, "Digite uma mensagem mais longa!")
+      .max(60, "Digite uma mensagem mais curta!")
+      .nullable(),
+  });
 
   const allowSubmit = (isTouched, isValid) => {
     if (isTouched && isValid) {
-      return true
+      return true;
     }
-  
-    return false
-  }
+
+    return false;
+  };
 
   const processMessage = (message) => {
-    let newMessage = message
+    let newMessage = message;
 
-    newMessage = newMessage.replace(/\s/g, '%20')
-    console.log(newMessage)
-    return newMessage
-  }
+    newMessage = newMessage.replace(/\s/g, "%20");
+    console.log(newMessage);
+    return newMessage;
+  };
 
   const generateLink = (values) => {
-    const baseUrl = 'https://api.whatsapp.com/send?'
+    const baseUrl = "https://api.whatsapp.com/send?";
 
-    let finalUrl = ''
+    let finalUrl = "";
 
     if (values.phoneNumber) {
-      finalUrl = `${baseUrl}phone=${values.phoneNumber}`
+      finalUrl = `${baseUrl}phone=${values.phoneNumber}`;
     }
 
     if (values.message) {
-      finalUrl = `${finalUrl}&text=${processMessage(values.message)}`
+      finalUrl = `${finalUrl}&text=${processMessage(values.message)}`;
     }
 
-    setWhatsLink(finalUrl)
+    setWhatsLink(finalUrl);
+  };
+
+  function copiarTexto() {
+    navigator.clipboard.writeText({ whatsLink }.whatsLink);
   }
 
   return (
@@ -67,54 +74,58 @@ function WhatsAppAPIGenerator () {
           generateLink(values);
         }}
       >
-
         {({ errors, touched, isValid }) => (
-
-         <Form>
-          <Row>
-            <Col md={12}>
-              {errors.phoneNumber && touched.phoneNumber ? (
-                <ErrorLabel>{errors.phoneNumber}</ErrorLabel>
-              ) : null}
-              <Field name="phoneNumber" className="form-control mb-3"/>
-            </Col>
-          </Row>
-          
-           <Row>
-            <Col md={12}>
-              {errors.message && touched.message ? (
-                <ErrorLabel>{errors.message}</ErrorLabel>
-              ) : null}
-              <Field name="message" className="form-control mb-3"/>
-                
-            </Col>
-           </Row>
-           
-           <Button type="submit" disabled={!allowSubmit(touched, isValid)}>Gerar</Button>
-
-          {whatsLink && (
+          <Form>
             <Row>
               <Col md={12}>
-                <div className="mt-5 mb-1 ">Link gerado:</div>
-                <Alert variant='success'>
-                  {whatsLink}
-                </Alert>
+                {errors.phoneNumber && touched.phoneNumber ? (
+                  <ErrorLabel>{errors.phoneNumber}</ErrorLabel>
+                ) : null}
+                <label htmlFor="phoneNumber">Celular (whatsapp)</label>
+                <Field name="phoneNumber" className="form-control mb-3" />
               </Col>
             </Row>
-          )}
-         </Form>
-       )}
 
+            <Row>
+              <Col md={12}>
+                {errors.message && touched.message ? (
+                  <ErrorLabel>{errors.message}</ErrorLabel>
+                ) : null}
+                <label htmlFor="message">Mensagem</label>
+                <Field name="message" className="form-control mb-3" />
+              </Col>
+            </Row>
+
+            <Button type="submit" disabled={!allowSubmit(touched, isValid)}>
+              Gerar
+            </Button>
+
+            {whatsLink && (
+              <Row>
+                <Col md={12}>
+                  <div className="mt-5 mb-1 ">Link gerado:</div>
+                  <Alert variant="success">
+                    {whatsLink}
+
+                    <div className="mt-2 text-center">
+                      <Button
+                        variant="info"
+                        className="fw-bold"
+                        onClick={() => copiarTexto()}
+                      >
+                        Copiar Texto
+                      </Button>
+                    </div>
+                  </Alert>
+                </Col>
+              </Row>
+            )}
+          </Form>
+        )}
       </Formik>
-
-      
-      
-
-
     </Container>
-  )
+  );
 }
 
-export {WhatsAppAPIGenerator}
-export default WhatsAppAPIGenerator
-
+export { WhatsAppAPIGenerator };
+export default WhatsAppAPIGenerator;
